1611b618757e921eaf7e80cdc1b1fe499a77b695
Juan Madrigal Vergel <juan.madrigal@ciberexperis.es>
2018-10-10 21:02:23 +0200

    loging page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+
+// import { User } from '../../providers';
+// import { MainPage } from '../';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  // The account fields for the login form.
+  // If you're using the username field with or without email, make
+  // sure to add it to the type
+  account: { email: string, password: string } = {
+    email: 'appcollaboration2018@gmail.com',
+    password: 'ionicAppGroup'
+  };
+
+  // Our translated text strings
+  private loginErrorString: string;
+
+  constructor(public navCtrl: NavController,
+    public toastCtrl: ToastController) {
+
+    /* this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+      this.loginErrorString = value;
+    }) */
+  }
+
+  // Attempt to login in through our User service
+  doLogin() {
+    /* this.user.login(this.account).subscribe((resp) => {
+      // this.navCtrl.push(MainPage);
+    }, (err) => {
+      this.navCtrl.push(MainPage);
+      // Unable to log in
+      let toast = this.toastCtrl.create({
+        message: this.loginErrorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    }); */
+    console.log('login');
+  }
+}
