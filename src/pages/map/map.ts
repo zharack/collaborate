7e3b66531ff37b7e1d8b9fea6d76c377e46be8fe
Javier Montero Fernandez <JAVIER.MONTERO@CIBEREXPERIS.ES>
2019-01-23 20:12:38 +0100

    anadido places a falta de check de la key

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+//https://ionicthemes.com/tutorials/about/ionic-2-google-maps-google-places-geolocation
+// Quitar el import de los elementos y dejar solo la carga de google en indexhtml
 import {
   GoogleMaps,
   GoogleMap,
@@ -17,7 +19,7 @@ import { GeoManagerProvider, GeoManagerModel } from '../../providers/geo-manager
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+declare var google : any;
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
@@ -26,8 +28,59 @@ export class MapPage {
   map: GoogleMap;
   mapLoaded = false;
   subs;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geoManager:GeoManagerProvider) { // , public googleMaps:GoogleMaps
+
+  public autocomplete = {
+    input:''
   }
+  public autocompleteItems = [];
+  public GoogleAutocomplete;
+  public geocoder;
+  public markers = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geoManager:GeoManagerProvider) { // , public googleMaps:GoogleMaps
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.geocoder = new google.maps.Geocoder;
+  }
+
+
+  updateSearchResults(results){
+    console.log('updateSearchResults', results);
+    debugger;
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+    (predictions, status) => {
+      this.autocompleteItems = [];
+      // this.zone.run(() => {
+        predictions.forEach((prediction) => {
+          this.autocompleteItems.push(prediction);
+        });
+      // });
+    });
+  }
+
+  selectSearchResult(item){
+    // this.clearMarkers();
+    this.autocompleteItems = [];
+  
+    this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
+      if(status === 'OK' && results[0]){
+        let position = {
+            lat: results[0].geometry.location.lat,
+            lng: results[0].geometry.location.lng
+        };
+        let marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: this.map,
+        });
+        this.markers.push(marker);
+        // this.map.setCenter(results[0].geometry.location);
+      }
+    })
+  }
+  
 
   ionViewWillUnload(){
     alert(123);
@@ -40,8 +93,8 @@ export class MapPage {
   loadMap(): any {
         // This code is necessary for browser
     Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBbuIOXmkpNn0x8dypFRAaSzJsUi9DxsPo',
-      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBbuIOXmkpNn0x8dypFRAaSzJsUi9DxsPo'
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyCvOaaXFM3Of838uif4O450lUNBt57JxD4',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyCvOaaXFM3Of838uif4O450lUNBt57JxD4'
     });
 
 
